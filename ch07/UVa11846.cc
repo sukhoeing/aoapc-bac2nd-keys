237b675df561366fd475230b6830eb14b7cf2ad8
Feng Chen <sukhoeing@gmail.com>
2017-12-03 20:18:15 +0800

    UVa11846 Finding Seats Again 代码更新

--- ch07/UVa11846.cc
+++ ch07/UVa11846.cc
@@ -1,17 +1,17 @@
 // UVa11846 Finding Seats Again
 // 陈锋
+#include <bitset>
 #include <cassert>
-#include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <functional>
-#include <iomanip>
+#include <climits>
 #include <iostream>
+#include <sstream>
 #include <list>
-#include <queue>
 #include <set>
+#include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <valarray>
@@ -20,76 +20,63 @@
 using namespace std;
 #define _for(i, a, b) for (int i = (a); i < (b); ++i)
 #define _rep(i, a, b) for (int i = (a); i <= (b); ++i)
-
+#define _zero(D) memset((D), 0, sizeof(D))
+#define _init(D, v) memset((D), (v), sizeof(D))
+#define _ri1(x) scanf("%d", &(x))
+#define _ri2(x, y) scanf("%d%d", &(x), &(y))
+#define _ri3(x, y, z) scanf("%d%d%d", &(x), &(y), &(z))
+#define _ri4(a, b, c, d) scanf("%d%d%d%d", &(a), &(b), &(c), &(d))
 typedef long long LL;
-const int MAXN = 20, MAXK = 32;
+
+const int MAXN = 20;
 int N, K;
-char G[MAXN][MAXN];
-
-struct Group {
-  int leaderX, leaderY, size, ch;
-  bool operator<(const Group& g) const { return size > g.size; }
-};
-vector<Group> groups;
-
-bool dfs(int ki) {
-  if (ki == K) return true;
-  const auto& kg = groups[ki];
-  int kx = kg.leaderX, ky = kg.leaderY;
-  auto isEmpty = [&](int x, int y) { return !isalpha(G[x][y]) || G[x][y] == kg.ch; };
-  _rep(H, 1, kg.size) if (kg.size % H == 0) {  // height of the rec
-    int W = kg.size / H;                       // width of the rec
-    _rep(rx, max(0, kx - H + 1), min(kx, N - H)) _rep(ry, max(0, ky - W + 1), min(ky, N - W)) {
-      bool valid = isEmpty(rx, ry) && isEmpty(rx + H - 1, ry) && isEmpty(rx, ry + W - 1) &&
-                   isEmpty(rx + H - 1, ry + W - 1);
-      if (!valid) continue;
-      _for(x, rx, rx + H) {
-        _for(y, ry, ry + W) {
-          if (isEmpty(x, y)) {
-            G[x][y] = kg.ch;
-          } else {
-            valid = false;
+char G[MAXN][MAXN], Ans[MAXN][MAXN];
+bool dfs(const char c, int pos) { // 当前决策的格子编号，以及所要放置的字符
+  while(pos != N*N && Ans[pos/N][pos%N] != '.') pos++; // 忽略已经决策过的格子
+  if(pos == N*N) return true;
+  int x1 = pos%N, y1 = pos/N; // uppper-left corner
+  _for(y2, y1, N) _for(x2, x1, N){ // bottom-right corner
+    int lcnt = 0, area = (y2 - y1 + 1) * (x2 - x1 + 1); // leader count, area
+    if(area > 9 || Ans[y2][x2] != '.') break; // max area ≤ 9
+    int ld = -1; // leader digit
+    bool used = false, too_large = false;
+    _rep(y, y1, y2) { // every cell
+      _rep(x, x1, x2){
+        char gc = G[y][x];
+        if(isdigit(gc)) {
+          lcnt++, ld = gc - '0';
+          // too many leaders, area too large, width invalid
+          if(lcnt > 1 || area > ld || ld%(y2-y1+1)) {
+            too_large = true;
             break;
           }
+        }     
+        if(Ans[y][x] != '.') {
+          used = true;
+          break;
         }
-        if (!valid) break;
       }
-
-      if (valid && dfs(ki + 1)) return true;
-      _for(x, rx, rx + H) _for(y, ry, ry + W) if (G[x][y] == kg.ch) G[x][y] = '_';
-      G[kx][ky] = kg.ch;
+      if(used || too_large) break;
     }
+    if(used || too_large) break;
+    if(lcnt == 0 || ld > area) continue; // area too small, continue trying
+    _rep(y, y1, y2) _rep(x, x1, x2) Ans[y][x] = c; // fill the region
+    if(dfs(c+1, pos+(x2-x1+1))) return true; // go to right of the rectangle
+    _rep(y, y1, y2) _rep(x, x1, x2) Ans[y][x] = '.';
   }
   return false;
 }
 
 int main() {
-  char buf[MAXK];
-  while (scanf("%d%d", &N, &K) == 2 && N && K) {
-    memset(G, 0, sizeof(G));
-    groups.clear();
-    _for(r, 0, N) {
-      scanf("%s", buf);
-      _for(c, 0, N) {
-        G[r][c] = '_';
-        if (!isdigit(buf[c])) continue;
-        G[r][c] = groups.size() + 'A';
-        groups.push_back({r, c, buf[c] - '0', G[r][c]});
-      }
+  while(_ri2(N, K) == 2 && N && K){
+    _init(Ans, '.');
+    _for(i, 0, N) _for(j, 0, N) scanf(" %c ", &G[i][j]);
+    dfs('A', 0);
+    _for(i, 0, N) {
+      _for(j, 0, N) putchar(Ans[i][j]);
+      puts("");
     }
-
-    sort(begin(groups), end(groups));
-    assert(dfs(0));
-    _for(r, 0, N) puts(G[r]);
   }
   return 0;
 }
-// http://d.hatena.ne.jp/shioshiota/20110519/1305818334
-/*
-search.
-Since the answer is surely found,
-Take a look from the top left,
-If there is an uncertain mass, make a rectangle with the square at the upper left appropriately.
-Since the area is up to 9, there are not many candidates so far,
-The answer finishes earlier because it always finds it.
-*/
+// 20432842 11846 Finding Seats Again Accepted  C++11 0.000 2017-12-02 11:46:07
