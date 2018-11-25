90b000e4d4e8899e2e723f086e2f7cec10bbec4d
sukhoeing <sukhoeing@gmail.com>
2018-11-25 16:47:16 +0800

    Update UVa1674.cc

--- ch12/UVa1674.cc
+++ ch12/UVa1674.cc
@@ -1,5 +1,5 @@
 // UVa1674 Lightning Energy Report
-// 吴鑫烜 陈锋
+// 吴鑫烜 陈锋 魏子豪
 #include <bitset>
 #include <cassert>
 #include <cstdio>
@@ -17,7 +17,6 @@
 #include <unordered_set>
 #include <valarray>
 #include <vector>
-
 using namespace std;
 #define _for(i, a, b) for (int i = (a); i < (int)(b); ++i)
 #define _rep(i, a, b) for (int i = (a); i <= (int)(b); ++i)
@@ -29,81 +28,40 @@ using namespace std;
 #define _ri4(a, b, c, d) scanf("%d%d%d%d", &(a), &(b), &(c), &(d))
 typedef long long LL;
 
-struct NodeInfo { int minv, maxv, sumv; };
-NodeInfo operator+(const NodeInfo& n1, const NodeInfo& n2) 
-{ return {min(n1.minv, n2.minv), max(n1.maxv, n2.maxv), n1.sumv + n2.sumv}; }
-template<size_t SZ, int INF = 0x7f7f7f7f>
-struct IntervalTree {
-  NodeInfo nodes[SZ * 2];
-  int setv[SZ * 2], addv[SZ * 2], qL, qR, N;
-  bitset < SZ * 2 > isSet;
-  inline void setFlag(int o, int v) { setv[o] = v, isSet.set(o), addv[o] = 0; }
-  void init(int n) {
-    N = n;
-    int sz = N * 2 + 2;
-    fill_n(addv, sz, 0), isSet.reset(), isSet.set(1);
-    memset(nodes, 0, sizeof(NodeInfo) * sz);
-  }
-
-  inline void maintain(int o, int L, int R) { // 维护信息
-    int lc = o * 2, rc = o * 2 + 1, a = addv[o], s = setv[o];
-    auto &nd = nodes[o], &li = nodes[lc], &ri = nodes[rc];
-    if (R > L) nd = li + ri;
-    if (isSet[o]) nd = {s, s, s * (R - L + 1)};
-    if (a) nd.minv += a, nd.maxv += a, nd.sumv += a * (R - L + 1);
-  }
-
-  inline void pushdown(int o) { // 标记传递
-    int lc = o * 2, rc = o * 2 + 1;
-    if (isSet[o])
-      setFlag(lc, setv[o]), setFlag(rc, setv[o]), isSet.reset(o); // 清除本结点标记
-    if (addv[o])
-      addv[lc] += addv[o], addv[rc] += addv[o], addv[o] = 0; // 清除本结点标记
-  }
-
-  void update(int o, int L, int R, int op, int v) { // op(1:add, 2:set)
-    // printf("update %d(%d,%d),%d\n", o, L, R, v);
-
-    int lc = o * 2, rc = o * 2 + 1, M = L + (R - L) / 2;
-    if (qL <= L && qR >= R) { // 标记修改
-      if (op == 1) addv[o] += v; // add
-      else setFlag(o, v); // set
-    } else {
-      pushdown(o);
-      if (qL <= M) update(lc, L, M, op, v); else maintain(lc, L, M);
-      if (qR > M) update(rc, M + 1, R, op, v); else maintain(rc, M + 1, R);
-    }
-    maintain(o, L, R);
-  }
-
-  NodeInfo query(int o, int L, int R) {
-    int lc = o * 2, rc = o * 2 + 1, M = L + (R - L) / 2;
-    maintain(o, L, R);
-    if (qL <= L && qR >= R) return nodes[o];
-
-    pushdown(o);
-    NodeInfo li = {INF, -INF, 0}, ri = {INF, -INF, 0};
-    if (qL <= M) li = query(lc, L, M); else maintain(lc, L, M);
-    if (qR > M) ri = query(rc, M + 1, R); else maintain(rc, M + 1, R);
-    return li + ri;
-  }
-
-  NodeInfo query(int L, int R) {
-    qL = L, qR = R;
-    const auto& ni = query(1, 1, N);
-    return ni;
-  }
-  void add(int L, int R, int val) {
-    qL = L, qR = R;
-    update(1, 1, N, 1, val);
-  }
-};
-
 const int MAXN = 65536;
-//Fa[i]为i的父节点, HcHead[i]为i所在重链头, HSon[i]为i的重儿子, SZ[i]为子树i体积, ID[i]为i在线段树中序号
-int Fa[MAXN], HcHead[MAXN], Depth[MAXN], HSon[MAXN], SZ[MAXN], ID[MAXN];
-vector<int> G[MAXN]; //存储图
+//Fa[i]为i的父节点,HcHead[i]为i所在重链头,HSon[i]为i的重儿子,SZ[i]为子树i体积,ID[i]为i在线段树中序号
+int Fa[MAXN], HcHead[MAXN], Depth[MAXN], HSon[MAXN], SZ[MAXN], ID[MAXN],N,add[MAXN*4],intSz;
 
+void Tree_update(int o,int L,int R,int qL,int qR,int val){
+	if(qL <= L && R <= qR){
+		add[o] += val;	//区间加上val 
+		return ;
+	}
+	int M = L + R >> 1;
+	if(qL <= M) Tree_update(o<<1,L,M,qL,qR,val);//覆盖左区间 
+	if(M < qR) Tree_update(o<<1|1,M+1,R,qL,qR,val);//覆盖右区间 
+}
+
+void Tree_add(int qL,int qR,int val){
+	Tree_update(1,1,N,qL,qR,val);
+}
+
+void Tree_init(int o,int L,int R){//初始化线段树 
+	add[o] = 0;
+	if(L == R)	return;
+	int M = L + R >> 1;
+	Tree_init(o<<1,L,M);
+	Tree_init(o<<1|1,M+1,R);
+}
+
+int Tree_query(int o,int L,int R,int qv,int addv){ 
+	if(L == R)	return addv + add[o];//找到答案并且将答案返回 
+	int M = L + R >> 1;
+	if(qv <= M)return Tree_query(o<<1,L,M,qv,addv+add[o]);//答案在左区间 
+	else return Tree_query(o<<1|1,M+1,R,qv,addv+add[o]);//答案在右区间
+}
+
+vector<int> G[MAXN]; //存储图
 int dfs(int u, int fa) { //第一次dfs, 得到每个节点的重儿子, 深度, 和父节点
   SZ[u] = 1, Fa[u] = fa, HSon[u] = 0, Depth[u] = Depth[fa] + 1;
   for (auto v : G[u]) {
@@ -113,32 +71,29 @@ int dfs(int u, int fa) { //第一次dfs, 得到每个节点的重儿子, 深度,
   }
   return SZ[u];
 }
-
-void hld(int u, int fa, int x, int& intSz) { // 第二次dfs, 得到每个节点在线段树中的标号及所属重链的标号
+ void hld(int u, int fa, int x) { // 第二次dfs, 得到每个节点在线段树中的标号及所属重链的标号
   ID[u] = ++intSz, HcHead[u] = x; // 重链的标号为该重链最顶端的节点
-  if (HSon[u]) hld(HSon[u], u, x, intSz); // 先处理重链，保证剖分完之后每条重链中的标号是连续的
-  for (auto v : G[u]) if (v != fa && v != HSon[u])  hld(v, u, v, intSz);
+  if (HSon[u]) hld(HSon[u], u, x); // 先处理重链，保证剖分完之后每条重链中的标号是连续的
+  for (auto v : G[u]) if (v != fa && v != HSon[u])  hld(v, u, v);
 }
 
-IntervalTree<MAXN> intTree;
 void addPath(int u, int v, int w) {
   while (true) {
-    int hu = HcHead[u], hv = HcHead[v];
+    int hu = HcHead[u],hv = HcHead[v];
     if (hu == hv) break; // 直到两点位于同一条重链才停止
     if (Depth[hu] < Depth[hv]) swap(u, v), swap(hu, hv); // 更新h→head()
-    intTree.add(ID[hu], ID[u], w), u = Fa[hu];
+    Tree_add(ID[hu], ID[u], w),u = Fa[hu];
   }
   if (Depth[u] < Depth[v]) swap(u, v);
-  intTree.add(ID[v], ID[u], w); // 更新u->v
+  Tree_add(ID[v], ID[u], w); // 更新u->v
 }
-
-int main() {
-  int N, Q, T;
+ int main() {
+  int Q, T;
   scanf("%d", &T);
   for (int kase = 1; kase <= T; kase++) {
     scanf("%d", &N);
     assert(N < MAXN);
-    intTree.init(N);
+    Tree_init(1,1,N);
     for (int i = 1; i <= N; i++) G[i].clear();
     int u, v, w;
     SZ[0] = 0, Depth[1] = 0;
@@ -148,17 +103,21 @@ int main() {
       G[u].push_back(v), G[v].push_back(u);
     }
     dfs(1, 1);
-    int intSz = 0;
-    hld(1, 1, 1, intSz);
+    intSz = 0;
+    hld(1, 1, 1);
     scanf("%d", &Q);
     for (int i = 0; i < Q; i++) {
       scanf("%d%d%d", &u, &v, &w);
-      u++, v++;
-      addPath(u, v, w);
+      u++,v++;
+      addPath(u,v,w);
     }
     printf("Case #%d:\n", kase);
-    for (int i = 1; i <= N; i++) printf("%d\n", intTree.query(ID[i], ID[i]).sumv);
+    for (int i = 1; i <= N; i++) printf("%d\n", Tree_query(1,1,N,ID[i],0));
   }
   return 0;
 }
-// 22285530 1674  Lightning Energy Report Accepted  C++11 0.330 2018-11-10 15:29:54
+// 17025470 1674  Lightning Energy Report Accepted  C++11 0.130 2018-11-15 20:15:31
+//魏子豪重构版本 
+//改变一：重写了原代码中的线段树代码，减去了冗余代码和冗余功能 
+//改变二：减去了原代码部分函数传递参数数量改用全局变量代替
+//代码长度由原本的5354B 减少至3629 运行时间由380ms减少到130ms 
