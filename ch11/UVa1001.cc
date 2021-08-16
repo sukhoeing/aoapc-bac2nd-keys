c03ebfaba7290e52528e29dd644a1f3152697440
Feng Chen <sukhoeing@gmail.com>
2021-08-16 18:45:10 +0800

    代码简化

--- ch11/UVa1001.cc
+++ ch11/UVa1001.cc
@@ -1,80 +1,40 @@
 // UVa1001,Say Cheese, ACM/ICPC World Finals 2001
 // 陈锋
-#include<cassert>
-#include<cstdio>
-#include<cmath>
-#include<functional>
-#include<algorithm>
-#include<cstring>
-#include<vector>
-#include<set>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
 using namespace std;
-const int MAXN = 100 + 5;
-const double eps = 1e-8;
-int dcmp(double x) { if(fabs(x) < eps) return 0; else return x < 0 ? -1 : 1; }
-double read_double(){ double x; scanf("%lf", &x); return x; }
-const double PI = acos(-1.0);
+const int NN = 100 + 5;
 struct Point3 {
   double x, y, z;
-  Point3(double x=0, double y=0, double z=0):x(x),y(y),z(z) { }   
+  Point3(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
+  double dist(const Point3 &p) const {
+    return sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y) +
+                (z - p.z) * (z - p.z));
+  }
 };
-
-typedef Point3 Vector3;
-
-Vector3 operator + (const Vector3& A, const Vector3& B) { return Vector3(A.x+B.x, A.y+B.y, A.z+B.z); }
-Vector3 operator - (const Point3& A, const Point3& B) { return Vector3(A.x-B.x, A.y-B.y, A.z-B.z); }
-double Dot(const Vector3& A, const Vector3& B) { return A.x*B.x + A.y*B.y + A.z*B.z; }
-double Length(const Vector3& A) { return sqrt(Dot(A, A)); }
-
-void readPoint3(Point3& p) {
-    p.x = read_double();
-    p.y = read_double();
-    p.z = read_double();
+istream &operator>>(istream &is, Point3 &p) { return is >> p.x >> p.y >> p.z; }
+Point3 C[NN];            // 球心
+double R[NN], D[NN][NN]; // 半径, 两球心最短路
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0);
+  for (int n, t = 1; cin >> n && n > 0;) {
+    for (int i = 0; i < n; i++)
+      cin >> C[i] >> R[i];
+    cin >> C[n], R[n++] = 0;
+    cin >> C[n], R[n++] = 0;
+    for (int i = 0; i < n; i++) {
+      D[i][i] = 0;
+      for (int j = i + 1; j < n; j++)
+        D[i][j] = D[j][i] = 10 * max(0.0, C[i].dist(C[j]) - R[i] - R[j]);
+    }
+    for (int k = 0; k < n; k++)
+      for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+          D[i][j] = min(D[i][k] + D[k][j], D[i][j]);
+    printf("Cheese %d: Travel time = %.0lf sec\n", t++, D[n - 1][n - 2]);
+  }
+  return 0;
 }
-
-struct Sphere{ 
-    Point3 center; double radius; 
-    void readin() {
-        readPoint3(center);
-        radius = read_double();
-    }
-    
-    double Distance(const Sphere& rhs) {
-        double ans = Length(center - rhs.center) - radius - rhs.radius;
-        if(dcmp(ans) < 0) ans = 0;
-        return ans * 10;
-    }
-};
-
-Sphere S[MAXN];
-double d[MAXN][MAXN];
-
-int main()
-{           
-    int n, t = 1;
-    while(scanf("%d", &n) == 1 && n > 0) {
-        for(int i = 0; i < n; i++) S[i].readin();
-        readPoint3(S[n].center); S[n++].radius = 0;        
-        readPoint3(S[n].center); S[n++].radius = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            d[i][i] = 0;
-            for(int j = i+1; j < n; j++)
-            {
-                d[i][j] = d[j][i] = S[i].Distance(S[j]);
-                // printf("n = %d, %d - %d: %.2lf\n",n,i,j, d[i][j]);
-            }
-        }
-        
-        for(int k = 0; k < n; k++)
-            for(int i = 0; i < n; i++)
-                for(int j = 0; j < n; j++)
-                    d[i][j] = min(d[i][k] + d[k][j], d[i][j]);
-        
-        printf("Cheese %d: Travel time = %.0lf sec\n", t++, d[n-1][n-2]);
-    }
-    return 0;
-}
-
-// 14805850	1001	Say Cheese	Accepted	C++	0.036	2015-01-15 10:13:16
+// 14805850	1001 Say Cheese	Accepted C++	0.036 2015-01-15 10:13:16
--- ch11/UVa10801.cc
+++ ch11/UVa10801.cc
@@ -1,135 +1,119 @@
 // UVa10801 Lift Hopping
 // 陈锋
-#include <cassert>
-#include <cstdio>
-#include <functional>
 #include <algorithm>
+#include <iostream>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <queue>
-#include <set>
-#define _for(i,a,b) for( int i=(a); i<(b); ++i)
-#define _rep(i,a,b) for( int i=(a); i<=(b); ++i)
 using namespace std;
-const int MAXN = 5, MAXK = 100, MAXP = MAXN*MAXK;
+const int MAXN = 5, MAXK = 100, MAXP = MAXN * MAXK;
 const int INF = 100000 + 10;
-
-struct Edge{ int from, to, dist; };
-struct HeapNode{
-    int d, u;
-    bool operator < (const HeapNode& rhs) const { return d > rhs.d; }
+struct Edge {
+  int from, to, dist;
 };
-template<int MAXSIZE>
-struct Dijkstra{
-    int n, m, d[MAXSIZE], p[MAXSIZE];
-    vector<Edge> edges;
-    vector<int> G[MAXSIZE];
-    bool done[MAXSIZE];
-    
-    void init(int n)
-    {
-        this->n = n;
-        for(int i = 0; i < n; i++) G[i].clear();
-        edges.clear();
-        m = 0;
+struct HeapNode {
+  int d, u;
+  bool operator<(const HeapNode &rhs) const { return d > rhs.d; }
+};
+template <int SZ> 
+struct Dijkstra {
+  int n, m, d[SZ], p[SZ];
+  vector<Edge> edges;
+  vector<int> G[SZ];
+  bool done[SZ];
+
+  void init(int n) {
+    this->n = n;
+    for (int i = 0; i < n; i++)
+      G[i].clear();
+    edges.clear(), m = 0;
+  }
+
+  void addEdge(int from, int to, int dist) {
+    edges.push_back((Edge){from, to, dist});
+    m = edges.size(), G[from].push_back(m - 1);
+  }
+
+  void dijkstra(int s) {
+    priority_queue<HeapNode> Q;
+    fill_n(d, n, INF);
+    d[s] = 0, fill_n(done, n, false), Q.push((HeapNode){0, s});
+    while (!Q.empty()) {
+      HeapNode x = Q.top();
+      Q.pop();
+      int u = x.u;
+      if (done[u])
+        continue;
+      done[u] = true;
+      const vector<int> &adj = G[u];
+      for (size_t i = 0; i < adj.size(); i++) {
+        Edge &e = edges[adj[i]];
+        int v = e.to;
+        if (d[v] > d[u] + e.dist)
+          d[v] = d[u] + e.dist, p[v] = adj[i], Q.push((HeapNode){d[v], v});
+      }
     }
-    
-    void addEdge(int from, int to, int dist)
-    {
-        edges.push_back((Edge){from, to, dist});
-        m = edges.size();        
-        G[from].push_back(m - 1);
-    }
-    
-    void dijkstra(int s)
-    {
-        priority_queue<HeapNode> Q;
-        fill_n(d, n, INF);
-        d[s] = 0;
-        fill_n(done, n, false);
-        Q.push((HeapNode){0, s});
-        while(!Q.empty()) {
-            HeapNode x = Q.top(); Q.pop();
-            int u = x.u;
-            if(done[u]) continue;
-            done[u] = true;
-            const vector<int>& adj = G[u];
-            for(int i = 0; i < adj.size(); i++)
-            {
-                Edge& e = edges[adj[i]];
-                int to = e.to;
-                if(d[to] > d[u] + e.dist) {
-                    d[to] = d[u] + e.dist;
-                    p[to] = adj[i];
-                    Q.push((HeapNode){d[to], to});
-                }
-            }
-        }
-    }
+  }
 };
 
-Dijkstra<MAXP+1> solver;
+Dijkstra<MAXP + 1> solver;
 int n, k, T[MAXN];
 vector<int> Level[MAXK];
 char buf[512];
-int readint() { int x; scanf("%d", &x); return x; }
-
 int solve() {
-    solver.init(n*MAXK+1);
-    _for(i, 0, n) T[i] = readint();
-    gets(buf);
-    _for(e, 0, n){
-        gets(buf);
-        istringstream iss(string(buf), istringstream::in);
-        bool first = true; 
-        int l1, l; // 上一层，当前层
-        while(iss>>l) { 
-            if(first) first = false;
-            else { 
-                int v = MAXK*e + l1, v2= MAXK*e + l;
-                int dist = (l-l1)*T[e];
-                solver.addEdge(v,v2,dist);
-                solver.addEdge(v2,v,dist);
-                // printf("[%d,%d]-%d\n",v, v2, dist);
-            }
-            Level[l].push_back(e);
-            l1 = l;
-        }
+  solver.init(n * MAXK + 1);
+  for (int i = 0; i < n; i++)
+    cin >> T[i];
+  cin.ignore();
+  string s;
+  for (int e = 0; e < n; e++) {
+    getline(cin, s);
+    istringstream iss(s, istringstream::in);
+    bool first = true;
+    int l1, l; // 上一层，当前层
+    while (iss >> l) {
+      if (first)
+        first = false;
+      else {
+        int v = MAXK * e + l1, v2 = MAXK * e + l, dist = (l - l1) * T[e];
+        solver.addEdge(v, v2, dist), solver.addEdge(v2, v, dist);
+      }
+      Level[l].push_back(e);
+      l1 = l;
     }
+  }
+  for (int i = 0; i < MAXK; i++) {
+    vector<int> &li = Level[i];
+    for (size_t j = 0; j < li.size(); j++) {
+      for (size_t m = j + 1; m < li.size(); m++) {
+        int e1 = li[j], e2 = li[m], v1 = e1 * MAXK + i, v2 = e2 * MAXK + i;
+        solver.addEdge(v1, v2, 60), solver.addEdge(v2, v1, 60);
+      }
+    }
+  }
 
-    _for(i, 0, MAXK) {
-        vector<int>& li = Level[i];
-        _for(j, 0, li.size()){
-            _for(m, j+1, li.size()) {
-                int e1 = li[j], e2 = li[m], v1 = e1*MAXK+i, v2 = e2*MAXK+i;
-                solver.addEdge(v1,v2,60);
-                solver.addEdge(v2,v1,60);
-                // printf("[%d,%d]-%d\n",v1, v2, 60);
-            }
-        }
-    }
-    
-    vector<int>& L0 = Level[0];
-    vector<int>& Lk = Level[k];
-    if(L0.empty() || Lk.empty()) return 0;
-    
-    int ans = INF;
-    for(auto i : L0) {
-        solver.dijkstra(i * 100);
-        for(auto j : Lk) ans = min(ans, solver.d[j*100 + k]);
-    }
-    
-    return ans;
+  vector<int> &L0 = Level[0], &Lk = Level[k];
+  if (L0.empty() || Lk.empty()) return 0;
+  int ans = INF;
+  for(size_t i = 0; i < L0.size(); i++){
+    solver.dijkstra(L0[i] * 100);
+    for(size_t j = 0; j < Lk.size(); j++)
+      ans = min(ans, solver.d[Lk[j] * 100 + k]);
+  }
+  return ans;
 }
 
-int main(){
-    while(scanf("%d %d\n", &n, &k) == 2) {
-        int ans = solve();
-        if(ans != INF) printf("%d\n", ans);
-        else puts("IMPOSSIBLE");
-    }    
-    return 0;
+int main() {
+  while (cin>>n>>k) {
+    int ans = solve();
+    if (ans != INF)
+      printf("%d\n", ans);
+    else
+      puts("IMPOSSIBLE");
+  }
+  return 0;
 }
 
-// 14805862	10801	Lift Hopping	Accepted	C++	0.039	2015-01-15 10:14:57
+// 14805862 10801 Lift Hopping  Accepted  C++ 0.039
+// 2015-01-15 10:14:57
