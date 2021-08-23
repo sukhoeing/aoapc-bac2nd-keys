b41244cca80da5d3d0a6dfdaeef59734f3d455c0
Feng Chen <sukhoeing@gmail.com>
2021-08-23 15:21:21 +0800

    习题代码简化。

--- ch09/UVa12589.cc
+++ ch09/UVa12589.cc
@@ -7,42 +7,29 @@ struct Point {
   Point(int x = 0, int y = 0): x(x), y(y) {}
 };
 typedef Point Vector;
-
-Vector operator+ (const Vector& A, const Vector& B) { return Vector(A.x + B.x, A.y + B.y); }
 bool operator< (const Point& p1, const Point& p2) { return atan2(p1.y, p1.x) > atan2(p2.y, p2.x); }
-const int MAXN = 50 + 2, MAXY = MAXN * MAXN;
-int N, K, H, F[MAXN][MAXN][MAXY];
+const int NN = 50 + 2, YY = NN * NN;
+int N, K, H, F[NN][NN][YY];
 vector<Vector> vs;
-// 令F(i,c,y)为已经决策过i个向量，选择了其中c个，折线的最高y坐标为y的时候，后续还能增加的最大面积。
-// 结果就是F(0, 0, 0)
+// F(i,c,y)为已经决策过i个向量，选择了其中c个，折线的最高y坐标为y的时候，后续还能增加的最大面积。
 int dp(int i, int c, int y) {
-  assert(i <= N);
-  assert(c <= K);
+  assert(i <= N), assert(c <= K);
   int& f = F[i][c][y];
   if (f >= 0) return f;
   if (i == N || c == K) return f = 0;
   f = dp(i + 1, c, y);
-  const Vector& v = vs[i];
-  f = max(f, dp(i + 1, c + 1, y + v.y) + (2 * y + v.y) * v.x);
-  return f;
+  return f = max(f, dp(i + 1, c + 1, y + vs[i].y) + (2 * y + vs[i].y) * vs[i].x);
 }
-
 int main() {
-  int t, T;
-  Vector v;
-  scanf("%d\n", &T);
-  for (t = 1; t <= T; t++) {
-    H = 0, vs.clear(), scanf("%d%d\n", &N, &K);
-    for (int i = 0; i < N; i++) {
-      scanf("%d%d\n", &(v.x), &(v.y)), assert(v.x || v.y);
-      vs.push_back(v);
-      H += v.y;
-    }
+  int T;
+  ios::sync_with_stdio(false), cin.tie(0);
+  cin>>T;
+  for (int t = 1; t <= T; t++) {
+    H = 0, vs.clear(), cin>>N>>K;
+    for (int i = 0, x, y; i < N; i++)
+      cin>>x>>y, vs.push_back(Vector(x,y)), H += y;
     sort(vs.begin(), vs.end());
-    for (int i = 0; i <= N; i++)
-      for (int j = 0; j <= K; j++)
-        for (int y = 0; y <= H; y++)
-          F[i][j][y] = -1;
+    memset(F, -1, sizeof(F));
     int ans = dp(0, 0, 0);
     printf("Case %d: %d\n", t, ans);
   }
